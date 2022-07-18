a7e01f045a42e725f9335a498a82171d230466ac
bharti085 <84670462+bharti085@users.noreply.github.com>
2022-07-18 15:14:34 +0530

    Update rssi.js

--- rssi.js
+++ rssi.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 80;
-const bodyParser = require("body-parser");
-const router = express.Router();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(router);
+const port = 5000;
+// const bodyParser = require("body-parser");
+// const router = express.Router();
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(router);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/',(req,res) => {
-    const x = req.body;
-    console.log(x);
+// app.post('/',(req,res) => {
+//     const x = req.body;
+//     console.log(x);
     
-})
+// })
 
 app.listen(process.env.port || port, () => {
   console.log(`Example app listening on port ${port}`)
