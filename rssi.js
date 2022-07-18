518ba7b01d72b4a6aff705c7d8db599cd9c170ae
bharti085 <84670462+bharti085@users.noreply.github.com>
2022-07-18 13:35:35 +0530

    Add files via upload

---
+++ rssi.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require("body-parser");
+const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(router);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/',(req,res) => {
+    const x = req.body;
+    console.log(x);
+    
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
